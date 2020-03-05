7f518aeef2b2f2ccceadae10b323d0429a95d838
Alexander Khomenko <homenkosasha13@gmail.com>
2020-03-05 13:28:58 +0200

    Updates

---
+++ precache-manifest.ec2d3eecd95d1f0f6478000a6fd11d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ea538604b699f67e2254a0493e7fd8",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "1033746763fe03a34ee9",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.afd7457a.chunk.css"
+  },
+  {
+    "revision": "df13de786877138604bd",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.2d9122e1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.2d9122e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1033746763fe03a34ee9",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.7baba152.chunk.js"
+  },
+  {
+    "revision": "abb1b3173251fa12dabb",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.79321017.js"
+  }
+]);
